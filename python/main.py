d3d3612f7de7a5f7e4686144859519a04583c9b8
Dylan Littler <dylan.littler@tutanota.com>
2018-09-30 12:44:21 +1000

    Implement stack data structure and call it in main.py
    Implementation is not Pythonic. Pushing and popping uses list indexes,
    and list is initialised to fixed size, rather than using append,
    or push and pop, which would make this project redundant.

---
+++ python/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import argparse
+import stack
+
+parser = argparse.ArgumentParser()
+parser.add_argument('integers', type=int, nargs='*',
+                    help='integers to be pushed onto the stack')
+args = parser.parse_args()
+
+test_stack = stack.Stack(len(args.integers))
+
+for item in args.integers:
+    test_stack.stack_push(item)
+
+for item in test_stack.stack_array:
+    print("Current item: {}".format(test_stack.stack_pop()))
---
+++ python/stack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" This program implements a stack data structure. """
+
+class Stack:
+
+    def __init__(self, stack_size):
+        self.stack_array = [0 for x in range(stack_size)]
+        self.top = 0
+    
+    def stack_push(self, item):
+        self.stack_array[self.top] = item
+        self.top += 1
+
+    def stack_pop(self):
+        self.top -= 1
+        last_item = self.stack_array[self.top]
+        return last_item
